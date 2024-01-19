d37bd7cb7dd02203f7e4694295f0dd81afac65cc
Roman <roman-la@users.noreply.github.com>
2024-01-19 17:22:39 +0100

    remove unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import Tab from '@mui/material/Tab'
 import TabContext from '@mui/lab/TabContext'
